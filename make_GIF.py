a560bb658ec4499c0b9c301365dfef5fc37c45b1
HyeokJong <jj770206@naver.com>
2022-04-09 04:06:45 +0900

    add resize

--- make_GIF.py
+++ make_GIF.py
@@ -9,7 +9,7 @@ def args():
     parser.add_argument("--GIF_dir", type = str, help = "GIF directory to saved")
     return parser.parse_args()
 
-def make_GIF(GIF_dir ,images_dir):
+def make_GIF(GIF_dir ,images_dir, resize = (500,500)):
     list_ = os.listdir(images_dir)
 
     for n in range(16):
@@ -18,10 +18,13 @@ def make_GIF(GIF_dir ,images_dir):
         for i in list_:
 
             if (i[0] == str(n)) or(i[0:2] == str(n)):
-                frames.append(Image.open(images_dir+"/"+i))
+                if resize:
+                    frames.append(Image.open(images_dir+"/"+i).resize(resize))
+                else:
+                    frames.append(Image.open(images_dir+"/"+i))
         GIF = frames[0]
         GIF.save(f"{GIF_dir}/{n}th.gif", format = "GIF", append_images = frames,
-        save_all = True, duration = 200, loop = 0, dpi=(300, 300))
+        save_all = True, duration = 100, loop = 0, dpi=(500, 500))
 
 
 
